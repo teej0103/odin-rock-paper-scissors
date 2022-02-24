478782c4c77e16b92b0031d237dac2bf31dba681
tj <tjciccone28@gmail.com>
2022-02-23 22:57:47 -0500

    created simple UI for game and using DOM selectors

--- script.js
+++ script.js
@@ -1,81 +1,79 @@
-
+// gets a random number between 1 and 3
 function getRandomNumber() {
     return Math.floor(Math.random() * 3)
 }
 
-function showScore() {
-    console.log(`Player: ${playerScore}\nComputer: ${computerScore}`)
+//disables all event listeners so player cant press button until play again is clicked
+function disableButtons() {
+    rockBtn.removeEventListener('click', playRound);
+    paperBtn.removeEventListener('click', playRound);
+    scissorsBtn.removeEventListener('click', playRound);
 }
 
+// displays final winner to console
+function displayWinner() {
+    let winningMessage = document.createElement('p');
+    let container = document.querySelector('.container');
+    if (playerScore === 5) {
+        winningMessage.innerText = "Congragulations, you beat the machine! Reload the page to play again";
+        container.appendChild(winningMessage);
+    } else {
+        winningMessage.innerText = "Oooof... You lost to a computer... reload the page if you want to play again";
+        container.appendChild(winningMessage);
+    }
 
+}
+
+//determine winner based on player choice and computer's selection
 function playRound() {
 
-    let playerSelection = prompt("Rock, Paper, or Scissors...What will it be?")
-    if(playerSelection && playerSelection.length > 0) {
-        playerSelection = playerSelection.toLowerCase();
-    }
-
+    // store both selections
+    let playerSelection = this.id;
     let computerSelection = winningPairs[getRandomNumber()][0]
 
-    if(winningPairs.find(el => el[0] === playerSelection && el[1] === computerSelection)) {
-        return 'win'
+    // determine winner of single round
+    if (winningPairs.find(el => el[0] === playerSelection && el[1] === computerSelection)) {
+        playerScore += 1;
+        playerScoreboard.innerText = playerScore;
+        message.innerText = `You Won! ${playerSelection} beats ${computerSelection}`;
+    } else if (computerSelection === playerSelection) {
+        message.innerText = `lame, you both chose ${playerSelection}`;
+    } else {
+        computerScore += 1;
+        computerScoreboard.innerText = computerScore;
+        message.innerText = `oh no, you lost! ${computerSelection} beats ${playerSelection}`;
     }
-    else if(computerSelection === playerSelection) {
-        return 'tie'
-    }
-    return 'lose'
-}
-
-function decideWinner(x, y) {
-    if(x > y) {
-        console.log("Congragulations, you beat the machine!")
-    }
-    else if (y > x) {
-        console.log("You lost to a computer...")
-    }
-    else {
-        console.log("you tied")
+    // check if 5 has been reached, disable buttons and display final winner if so
+    if (playerScore === 5 || computerScore === 5) {
+        disableButtons();
+        displayWinner();
     }
 }
 
-function playGame() {
+// variables to hold elements on page that display scores
+const playerScoreboard = document.querySelector('.player-score');
+const computerScoreboard = document.querySelector('.computer-score')
 
-    showScore();
+// selects each button on page and stores it in a variable
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
 
-    for( let i = 0; i < 5; i++) {
-        let outcome = playRound()
-        if(outcome === 'win') {
-            playerScore++;
-            console.log(`You win!`)
-            showScore(playerScore, computerScore);
-        }
-        else if(outcome === 'lose') {
-            computerScore++;
-            console.log(`You lose`)
-            showScore(playerScore, computerScore);
-        }
-        else {
-            console.log(`It's a tie`)
-            showScore(playerScore, computerScore);
-        }
-    }
+// event listeners for each choice (rock, paper, scissors)
+rockBtn.addEventListener('click', playRound);
+paperBtn.addEventListener('click', playRound);
+scissorsBtn.addEventListener('click', playRound);
 
-    decideWinner(playerScore, computerScore);
-}
+// multidimensional array containing all possible winning pairs for game
+const winningPairs = [
+    ['rock', 'scissors'],
+    ['paper', 'rock'],
+    ['scissors', 'paper']
+]
 
-    const winningPairs = [
-        ['rock', 'scissors'],
-        ['paper', 'rock'],
-        ['scissors', 'paper']
-    ]
+// variables for keeping track of scores
+let playerScore = 0;
+let computerScore = 0;
 
-    let playerScore = 0;
-    let computerScore = 0;
-
-    console.log("Welcome to Rock, Paper, Scissors...")
-    console.log("Want to play a quick best of five?")
-    let answer = prompt("Type yes to play");
-    if(answer && answer.length > 0) {
-        answer = answer.toLowerCase();
-        playGame();
-    }
+// selects and stores for game winner message output
+const message = document.querySelector('.message');
